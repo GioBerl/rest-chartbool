d88dcc0cbd983c8cf10632742b9a66a6a456c621
Gio <giovanni.berl1@gmail.com>
2020-06-11 00:08:12 +0200

    hover sui ezzi di torta

--- prove.js
+++ prove.js
@@ -145,20 +145,24 @@ $(document).ready(function () {
                             "rgba(255, 206, 86, 1)",
                             "rgba(75, 255, 192, 1)",
                         ],
+                        hoverBackgroundColor: [
+                            "rgba(255,99,132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(255, 206, 86, 1)",
+                            "rgba(75, 192, 192, 1)",
+                        ],
                         borderWidth: 1,
                     },
                 ],
             },
             options: {
-                // scales: {
-                //     yAxes: [
-                //         {
-                //             ticks: {
-                //                 beginAtZero: true,
-                //             },
-                //         },
-                //     ],
-                // },
+                title: {
+                    display: true,
+                    text: "Percentage Sales ",
+                },
+                legend: {
+                    position: "bottom",
+                },
             },
         });
     }
